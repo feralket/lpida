d3db43addf6736cee4192b88cac96857dac6d9cb
Mandip Rai <raister21@gmail.com>
2021-01-03 19:13:52 +0545

    Chaned about us

--- src/components/dashboard/about/About.js
+++ src/components/dashboard/about/About.js
@@ -7,11 +7,7 @@ function About() {
                 <img className="responsive-img" src="/icons/low_qualitiy_logo_invert.jpg" alt=""/>
                 <div className="card z-depth-1">
                     <div className="card-content">
-                        <p>Hello ma messi, ani ke cha ?</p>
-                        <p>Sab thikchani ? mero pani sab dikcha</p>
-                        <p>Khel ani ramro hudaicha ni</p>
-                        <p>dashain ma khasi katera khane ni ?</p>
-                        <p>La happy dashain sab lai, Jai nepal</p>
+                        <p>Beyond Hope</p>
                     </div>
                 </div>
             </div>
